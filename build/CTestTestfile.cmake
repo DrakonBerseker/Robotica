1997c453493e4ad4af6cfb84396c06175d82a3c9
Daniel Salcedo <deduardo.salcedo@gmail.com>
2022-07-01 16:48:44 -0500

    Primera prueba de git con robotica

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniel/Robotica/lab_ws/src/LabRobotica20221/lab1
+# Build directory: /home/daniel/Robotica/lab_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/LabRobotica20221/lab1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniel/Robotica/lab_ws/src/LabRobotica20221/lab1
+# Build directory: /home/daniel/Robotica/lab_ws/build/LabRobotica20221/lab1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
